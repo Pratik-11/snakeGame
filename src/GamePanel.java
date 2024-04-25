5a8d5a4d85a3d01785308f636bbb3f5cb81da164
Pratik S <pratiksingh.sskr@gmail.com>
2024-04-25 23:44:32 +0530

    more

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,23 +6,20 @@ import java.util.Random;
 import java.util.random.*;
 import javax.swing.JPanel.*;
 
-
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = 1L;
-
 
     // constants bro
     static final int Width = 500;
     static final int Height = 500;
     static final int Unit_Size = 20;
-    static final int Number_ofUnits = (Width*Height) / (Unit_Size * Unit_Size);
+    static final int Number_ofUnits = (Width * Height) / (Unit_Size * Unit_Size);
 
     final int[] x = new int[Number_ofUnits];
     final int[] y = new int[Number_ofUnits];
 
-
-    //Snake initial length and all
+    // Snake initial length and all
     int length = 5;
     int foodEaten;
     int foodX;
@@ -31,7 +28,6 @@ public class GamePanel extends JPanel implements ActionListener{
     boolean running = false;
     Random random;
     Timer timer;
-
 
     GamePanel() {
         random = new Random();
@@ -42,7 +38,7 @@ public class GamePanel extends JPanel implements ActionListener{
         play();
     }
 
-    public void play(){
+    public void play() {
         addFood();
         running = true;
 
@@ -51,91 +47,89 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     @Override
-    public void paintComponent(Graphics graphics){
+    public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         draw(graphics);
     }
 
-    public void move(){
-        for(int i = length; i>0 ; i--){
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+    public void move() {
+        for (int i = length; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
 
-        if(direction == 'L'){
+        if (direction == 'L') {
             x[0] = x[0] - Unit_Size;
-        }
-        else if(direction == 'R'){
+        } else if (direction == 'R') {
             x[0] = x[0] + Unit_Size;
-        }
-        else if(direction == 'U'){
+        } else if (direction == 'U') {
             x[0] = y[0] - Unit_Size;
-        }
-        else{
+        } else {
             y[0] = y[0] + Unit_Size;
         }
     }
 
-    public void checkFood(){
-        if(x[0] == foodX && y[0] == foodY){
+    public void checkFood() {
+        if (x[0] == foodX && y[0] == foodY) {
             length++;
             foodEaten++;
             addFood();
         }
     }
 
-    public void draw(Graphics graphics){
+    public void draw(Graphics graphics) {
 
         if (running) {
-            graphics.setColor(new Color (210, 115, 90));
-            graphics.fillOval (foodX, foodY, Unit_Size, Unit_Size);
+            graphics.setColor(new Color(210, 115, 90));
+            graphics.fillOval(foodX, foodY, Unit_Size, Unit_Size);
             graphics.setColor(Color.white);
             graphics.fillRect(x[0], y[0], Unit_Size, Unit_Size);
-            for (int i=1; i < length; i++) {
-            graphics.setColor(new Color (40, 200, 150));
-            graphics.fillRect(x[i], y[i], Unit_Size, Unit_Size);
+            for (int i = 1; i < length; i++) {
+                graphics.setColor(new Color(40, 200, 150));
+                graphics.fillRect(x[i], y[i], Unit_Size, Unit_Size);
             }
             graphics.setColor(Color.white);
             graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
-            FontMetrics metrics = getFontMetrics (graphics.getFont());
-            graphics.drawString("Score: "+foodEaten, (Width - metrics.stringWidth("Score: "+ foodEaten)) / 2, graphics.getFont().getSize());
-        } 
-            else {
+            FontMetrics metrics = getFontMetrics(graphics.getFont());
+            graphics.drawString("Score: " + foodEaten, (Width - metrics.stringWidth("Score: " + foodEaten)) / 2,
+                    graphics.getFont().getSize());
+        } else {
             gameOver(graphics);
         }
     }
 
-    public void addFood(){
-        foodX = random.nextInt((int)(Width/Unit_Size))*Unit_Size;
-        foodY= random.nextInt((int)(Width/Unit_Size))*Unit_Size;
+    public void addFood() {
+        foodX = random.nextInt((int) (Width / Unit_Size)) * Unit_Size;
+        foodY = random.nextInt((int) (Width / Unit_Size)) * Unit_Size;
     }
 
     public void checkHit() {
         // check if head cuts into its own body
 
         for (int i = length; i > 0; i--) {
-        if (x[0] ==  x[i] && y[0] == y[i]) {
-        running = false;
-        }
+            if (x[0] == x[i] && y[0] == y[i]) {
+                running = false;
+            }
         }
         // check if head run into the walls
 
         if (x[0] < 0 || x[0] > Width || y[0] < 0 || y[0] > HEIGHT) {
-        running = false;
+            running = false;
         }
-        if(!running) {
-        timer.stop();
+        if (!running) {
+            timer.stop();
         }
-        }
-        public void gameOver (Graphics graphics) {
+    }
+
+    public void gameOver(Graphics graphics) {
         graphics.setColor(Color.red);
-        graphics.setFont(new Font("Sans serif", Font. ROMAN_BASELINE, 50));
-        FontMetrics metrics =  getFontMetrics (graphics.getFont());
+        graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 50));
+        FontMetrics metrics = getFontMetrics(graphics.getFont());
         graphics.drawString("Game Over", (Width - metrics.stringWidth("Game Over")) / 2, HEIGHT / 2);
         graphics.setColor(Color.white);
         graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
         metrics = getFontMetrics(graphics.getFont());
-        graphics.drawString("Score: "+foodEaten, (Width - metrics.stringWidth("Score: "+foodEaten)) / 2, graphics.getFont().getSize());
-        }
+        graphics.drawString("Score: " + foodEaten, (Width - metrics.stringWidth("Score: " + foodEaten)) / 2,graphics.getFont().getSize());
+    }
 
 }
